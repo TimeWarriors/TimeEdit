a61f3795eb3ffc47c1e1762d4fe68e7615eb01cb
rs222kn <richard.soderman@gmail.com>
2016-03-16 13:24:57 +0100

    updated usageDemo.js

--- webbskrapa/usageDemo.js
+++ webbskrapa/usageDemo.js
@@ -3,13 +3,13 @@ const TimeEdidApi = require('./timeeditApi/index.js');
 const init = () => {
 
     // lnu
-    const timeEdidApiLnu = new TimeEdidApi(
+    /*const timeEdidApiLnu = new TimeEdidApi(
         'https://se.timeedit.net/web/lnu/db1/schema1/',
-        4
-    );
+        3
+    );*/
 
     // todays schedule
-    timeEdidApiLnu.getTodaysSchedule('ny105')
+    /*timeEdidApiLnu.getTodaysSchedule('ny105')
         .then((roomSchedule) => {
             console.log(JSON.stringify(roomSchedule, null ,2));
         }).catch((er) => {
@@ -35,6 +35,15 @@ const init = () => {
     // get the diffrent types of schedule
     TimeEdidApi.getAllTypes(
             'https://se.timeedit.net/web/lnu/db1/schema1/'
+        ).then((result) => {
+            console.log(JSON.stringify(result, null ,2));
+        }).catch((er) => {
+            console.log(er);
+        });*/
+
+
+    TimeEdidApi.getScheduleByScheduleUrl(
+            'https://se.timeedit.net/web/lnu/db1/schema1/s.html?i=6Y7XYQQ7wZ36QvZ5071875y7YQ8'
         ).then((result) => {
             console.log(JSON.stringify(result, null ,2));
         }).catch((er) => {
