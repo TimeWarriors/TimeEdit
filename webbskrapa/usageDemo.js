e7d9cd6e495fb0686710beeb7026f395f22434f9
rs222kn <richard.soderman@gmail.com>
2016-03-16 13:30:36 +0100

    removed comments from usageDemo.js

--- webbskrapa/usageDemo.js
+++ webbskrapa/usageDemo.js
@@ -3,13 +3,13 @@ const TimeEdidApi = require('./timeeditApi/index.js');
 const init = () => {
 
     // lnu
-    /*const timeEdidApiLnu = new TimeEdidApi(
+    const timeEdidApiLnu = new TimeEdidApi(
         'https://se.timeedit.net/web/lnu/db1/schema1/',
         3
-    );*/
+    );
 
     // todays schedule
-    /*timeEdidApiLnu.getTodaysSchedule('ny105')
+    timeEdidApiLnu.getTodaysSchedule('ny105')
         .then((roomSchedule) => {
             console.log(JSON.stringify(roomSchedule, null ,2));
         }).catch((er) => {
@@ -39,9 +39,9 @@ const init = () => {
             console.log(JSON.stringify(result, null ,2));
         }).catch((er) => {
             console.log(er);
-        });*/
+        });
 
-
+    // get schedule by schedule url
     TimeEdidApi.getScheduleByScheduleUrl(
             'https://se.timeedit.net/web/lnu/db1/schema1/s.html?i=6Y7XYQQ7wZ36QvZ5071875y7YQ8'
         ).then((result) => {
